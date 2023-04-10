829c6e83828147cdf491c0000cf9b1fd729fc4c4
ahmed3920 <Ahmed_Hisham_2103@fci.helwan.edu.eg>
2023-04-10 14:35:21 +0200

    remove unuseful comments

--- src/components/AddWareHouse.js
+++ src/components/AddWareHouse.js
@@ -28,68 +28,7 @@ function AddWareHouse(props) {
                     <Modal.Title>Add Warehouse</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <form
-                        
-                    >
-                        {/* <div className="md:flex md:items-center mb-6">
-                            <div className="md:w-1/3">
-                                <label
-                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                                    for="name"
-                                >
-                                    Full Name
-                                </label>
-                            </div>
-                            <div className="md:w-2/3">
-                                <input
-                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                                    id="name"
-                                    placeholder="John Smith"
-                                    type="text"
-                                    
-                                />
-                            </div>
-                        </div>
-                        <div className="md:flex md:items-center mb-6">
-                            <div className="md:w-1/3">
-                                <label
-                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                                    for="role"
-                                >
-                                    Role
-                                </label>
-                            </div>
-                            <div className="md:w-2/3">
-                                <input
-                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                                    id="role"
-                                    placeholder="Bank Teller"
-                                    type="text"
-                                    
-                                />
-                            </div>
-                        </div> */}
-                        {/* <div className="md:flex md:items-center mb-6">
-                            <div className="md:w-1/3">
-                                <label
-                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                                    for="img"
-                                >
-                                    Image URL
-                                </label>
-                            </div>
-                            <div className="md:w-2/3">
-                                <input
-                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                                    id="img"
-                                    placeholder="https://google.com"
-                                    type="text"
-                                    
-                                />
-                            </div>
-                        </div> */}
-                        <Form/>
-                    </form>
+                    <Form/>
                 </Modal.Body>
                 <Modal.Footer>
                     <button
