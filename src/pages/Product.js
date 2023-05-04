6ac22c5225c51e4fa533f0235498d719b4f179ea
Amr Yousef <3mr.g4mer.channel@gmail.com>
2023-05-04 22:02:09 +0300

    feat: fit products to new modal and add delete functionality

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -4,15 +4,22 @@ import ProductCard from '../components/ProductCard';
 import {useEffect, useState} from "react";
 import EditProductModal from "../components/EditProductModal";
 import SearchAdd from "../components/SearchAdd";
+import GeneralModal from "../components/GeneralModal";
 
+const formFields = [
+    {label: "Name:", type: "text", name: "name"},
+    {label: "Description:", type: "text", name: "description"},
+    {label: "Photo:", type: "text", name: "photo"},
+    {label: "Stock:", type: "number", name: "stock"}
+];
 
 function Product() {
     const [selectedProduct, setSelectedProduct] = useState(null);
-
     const [products, setProducts] = useState([]);
+    const [modalTitle, setModalTitle] = useState("");
 
     useEffect(() => {
-        fetch('http://localhost:3000/getProducts', {
+        fetch('http://localhost:3000/product', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -26,6 +33,7 @@ function Product() {
 
     function handleProductClick(product) {
         setSelectedProduct(product);
+        setModalTitle("Edit Product");
     }
 
     function handleCloseModal() {
@@ -40,8 +48,57 @@ function Product() {
             photo: "",
             stock: 0
         });
+        setModalTitle("Add Product");
+    }
 
-        console.log("Add product clicked!")
+    function handleProductDelete() {
+        if(!selectedProduct.product_id) return;
+        const url = `http://localhost:3000/product/${selectedProduct.product_id}`;
+        return fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': localStorage.getItem('sessionID')
+            },
+        })
+            .then(response => {
+                    if (response.ok) {
+                        window.location.reload();
+                    } else {
+                        console.error("Failed to delete product");
+                    }
+                }
+            )
+            .catch(error => {
+                    console.error("Failed to delete product", error);
+                }
+            );
+    }
+
+    function handleSubmitProduct(values) {
+
+        const url = selectedProduct.product_id ? `http://localhost:3000/product/${selectedProduct.product_id}` : "http://localhost:3000/product";
+        return fetch(url, {
+            method: selectedProduct.product_id ? "PUT" : "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': localStorage.getItem('sessionID')
+            },
+            body: JSON.stringify(values)
+        })
+            .then(response => {
+                    if (response.ok) {
+                        window.location.reload();
+                    } else {
+                        console.error("Failed to save product");
+                    }
+                }
+            )
+            .catch(error => {
+                    console.error("Failed to save product", error);
+                }
+            );
+
     }
 
     return (
@@ -63,7 +120,15 @@ function Product() {
                 </div>
             </div>
             {selectedProduct && (
-                <EditProductModal product={selectedProduct} onClose={handleCloseModal}/>
+                // <EditProductModal product={selectedProduct} onClose={handleCloseModal}/>
+                <GeneralModal
+                    title={modalTitle}
+                    formFields={formFields}
+                    initialValues={selectedProduct}
+                    onClose={handleCloseModal}
+                    onSubmit={handleSubmitProduct}
+                    onDelete= {selectedProduct.product_id ? handleProductDelete : null}
+                />
             )}
         </div>
     );
