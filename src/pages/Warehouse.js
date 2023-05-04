2c2e23080f33afe06dfffd930c1985df58e00e4a
Amr Yousef <3mr.g4mer.channel@gmail.com>
2023-05-04 16:44:13 +0300

    feat: add ability to add a new warehouse

--- src/pages/Warehouse.js
+++ src/pages/Warehouse.js
@@ -4,12 +4,58 @@ import WarehouseTable from "../components/WarehouseTable";
 import "../styles/WarehouseTable.css"
 import PageTitle from "../components/PageTitle";
 import SearchAdd from "../components/SearchAdd";
+import GeneralModal from "../components/GeneralModal";
+
+const initialValues = {
+    name: "",
+    location: "",
+    status: "inactive",
+    supervisorID: ""
+};
+
+const formFields = [
+    { label: "Name:", type: "text", name: "name" },
+    { label: "Location:", type: "text", name: "location" },
+    {
+        label: "Status:",
+        type: "select",
+        name: "status",
+        options: [
+            { label: "Active", value: "active" },
+            { label: "Inactive", value: "inactive" },
+        ],
+    },
+    { label: "Supervisor ID:", type: "number", name: "supervisorID" }
+];
+
+function handleWarehouseSubmit(warehouse) {
+    const url = warehouse.id ? `http://localhost:3000/warehouse/${warehouse.id}` : "http://localhost:3000/warehouse";
+    return fetch(url, {
+        method: warehouse.id ? "PUT" : "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': localStorage.getItem('sessionID')
+        },
+        body: JSON.stringify(warehouse)
+    })
+        .then(response => {
+            if (response.ok) {
+                // window.location.reload();
+            } else {
+                console.error("Failed to save warehouse");
+            }
+        })
+        .catch(error => {
+            console.error("Failed to save warehouse", error);
+        });
+}
 
 function Warehouse() {
     const [warehouses, setWarehouses] = useState([]);
+    const [showAddModal, setShowAddModal] = useState(false);
 
     useEffect(() => {
-        fetch('http://localhost:3000/allWarehouse', {
+        fetch('http://localhost:3000/warehouse', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -21,12 +67,41 @@ function Warehouse() {
             .catch(error => console.error(error));
     }, []);
 
+    const handleAddWarehouse = () => {
+        setShowAddModal(true);
+    }
+
+    const handleCloseModal = () => {
+        setShowAddModal(false);
+    }
+
+    const handleAddWarehouseSubmit = (values) => {
+        const newWarehouse = {
+            name: values.name,
+            location: values.location,
+            status: values.status,
+            supervisor_id: values.supervisorID
+        };
+
+        handleWarehouseSubmit(newWarehouse).then(r => console.log(r));
+        setShowAddModal(false);
+    }
+
     return (
         <div className="page-content">
             <PageTitle title="Warehouse" subtext="View/Manage"/>
             <div className="warehouse-table-block">
-                <SearchAdd />
+                <SearchAdd onAdd={handleAddWarehouse}/>
                 <WarehouseTable warehouses={warehouses} />
+                {showAddModal &&
+                    <GeneralModal
+                        title="Add Warehouse"
+                        formFields={formFields}
+                        initialValues={initialValues}
+                        onClose={handleCloseModal}
+                        onSubmit={handleAddWarehouseSubmit}
+                    />
+                }
             </div>
         </div>
     );
