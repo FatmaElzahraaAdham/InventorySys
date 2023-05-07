5809f273f4857965318ccbb520053a9486742d04
Heba <hiba30018@gmail.com>
2023-05-07 19:48:32 +0300

    feat: add Supervisor's pages

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,36 +1,58 @@
 import React from "react";
-import {AppstoreOutlined, BankOutlined, FileDoneOutlined, ShoppingCartOutlined, TeamOutlined, CheckSquareOutlined} from "@ant-design/icons";
+import { AppstoreOutlined, BankOutlined, FileDoneOutlined, ShoppingCartOutlined, TeamOutlined, CheckSquareOutlined } from "@ant-design/icons";
 
-export const SidebarData = [
-    {
-        title: "Dashboard",
-        icon: <AppstoreOutlined/>,
-        link: "/Dashboard",
-    },
-    {
-        title: "Warehouse",
-        icon: <BankOutlined/>,
-        link: "/Warehouse",
-    },
-    {
-        title: "Supervisors",
-        icon: <TeamOutlined/>,
-        link: "/Supervisor",
-    },
-    {
-        title: "Products",
-        icon: <ShoppingCartOutlined/>,
-        link: "/Product",
-    },
-    {
-        title: "Requests",
-        icon: <FileDoneOutlined/>,
-        link: "/Request",
-    },
-    {
-        title: "Assign",
-        icon: <CheckSquareOutlined />,
-        link: "/Productassign",
-    }
+var SidebarData = [];
 
-]
+function handleType() {
+  return localStorage.getItem('sessionID') !== '1';
+}
+
+if (handleType()) {  // if user is Supervisor
+  SidebarData = [
+    {
+      title: "Products",
+      icon: <ShoppingCartOutlined />,
+      link: "/Supervisor/products",
+    },
+    {
+      title: "Requests",
+      icon: <FileDoneOutlined />,
+      link: "/Supervisor/Requests",
+    },
+  ];
+} else {
+  SidebarData = [
+    {
+      title: "Dashboard",
+      icon: <AppstoreOutlined />,
+      link: "/Dashboard",
+    },
+    {
+      title: "Warehouse",
+      icon: <BankOutlined />,
+      link: "/Warehouse",
+    },
+    {
+      title: "Supervisors",
+      icon: <TeamOutlined />,
+      link: "/Supervisor",
+    },
+    {
+      title: "Products",
+      icon: <ShoppingCartOutlined />,
+      link: "/Product",
+    },
+    {
+      title: "Requests",
+      icon: <FileDoneOutlined />,
+      link: "/Request",
+    },
+    {
+      title: "Assign",
+      icon: <CheckSquareOutlined />,
+      link: "/Productassign",
+    },
+  ];
+}
+
+export default SidebarData;
---
+++ src/pages/Supervisor_Request.js
@@ -0,0 +1,13 @@
+import PageTitle from "../components/PageTitle";
+
+function Supervisor_Request() {
+
+    return (
+        <div className="page-content">
+            <PageTitle title="Requests" subtext="View/Manage"/>
+        </div>
+    );
+}
+
+
+export default Supervisor_Request;
