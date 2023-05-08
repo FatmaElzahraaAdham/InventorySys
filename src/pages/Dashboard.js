ae04dbf84911550fdd6fe733b75e5f826128cda8
Amr Yousef <3mr.g4mer.channel@gmail.com>
2023-05-08 17:34:41 +0300

    feat: display latest updates in the dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,53 +1,80 @@
-
 import "../styles/DashboardStyle.css"
 import PageTitle from "../components/PageTitle";
 import Activity from "../components/Activity";
+import {useEffect, useState} from "react";
 
-
-const activityList = [
-    {
-        id: 1,
-        title: "Activity Name 1",
-        cardsData: [
-            { id: 1, image: "https://picsum.photos/200", name: "Warehouse 15", title: "Ordered", subtext: "Order No.15" },
-            { id: 2, image: "https://picsum.photos/200", name: "Warehouse 15", title: "Ordered", subtext: "" },
-            { id: 3, image: "https://picsum.photos/200", name: "Warehouse 15", title: "Ordered", subtext: "Order No.15" }
-        ]
-    },
-    {
-        id: 2,
-        title: "Activity Name 2",
-        cardsData: [
-            { id: 1, image: "https://picsum.photos/200", name: "Warehouse 10", title: "Ordered", subtext: "Order No.15" },
-            { id: 2, image: "https://picsum.photos/200", name: "Warehouse 10", title: "Ordered", subtext: "" },
-            { id: 3, image: "https://picsum.photos/200", name: "Warehouse 10", title: "Ordered", subtext: "Order No.15" }
-        ]
-    },
-    {
-        id: 3,
-        title: "Activity Name 3",
-        cardsData: [
-            { id: 1, image: "https://picsum.photos/200", name: "Warehouse 20", title: "Ordered", subtext: "Order No.15" },
-            { id: 2, image: "https://picsum.photos/200", name: "Warehouse 20", title: "Ordered", subtext: "" },
-            { id: 3, image: "https://picsum.photos/200", name: "Warehouse 20", title: "Ordered", subtext: "Order No.15" }
-        ]
-    },
-]
 
 function Dashboard() {
+    const [products, setProducts] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    function fetchProducts() {
+        fetch('http://localhost:3000/product', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('sessionID')
+            },
+        })
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(error => console.error(error));
+    }
+
+    function fetchRequests() {
+        fetch('http://localhost:3000/request', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('sessionID')
+            },
+        })
+            .then(response => response.json())
+            .then(data => setRequests(data))
+            .catch(error => console.error(error));
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchRequests();
+    }, []);
+
+    const productList = products.slice(Math.max(products.length - 3, 0)).reverse();
+    const latestProducts = {
+        id: 1,
+        title: "Latest products",
+        cardsData: productList.map((product) => ({
+            id: product.product_id,
+            image: product.photo,
+            name: product.name,
+            title: `Stock: ${product.stock}`,
+            subtext: `ID: ${product.product_id}`
+        }))
+    };
+    console.log(requests)
+
+    const requestList = requests.slice(Math.max(requests.length - 3, 0)).reverse();
+    const latestRequests = {
+        id: 2,
+        title: "Latest requests",
+        cardsData: requestList.map((request) => ({
+            id: request.request_id,
+            image: "http://127.0.0.1:8080/in-stock.png",
+            name: `Supervisor ID: ${request.supervisor_id}`,
+            title: request.status,
+            subtext: request.date
+        }))
+    };
+
     return (
         <div className="page-content">
-            <PageTitle title="Dashboard" subtext="Home/Summary" />
+            <PageTitle title="Dashboard" subtext="Home/Summary"/>
             <div className="small-activity">
-                {activityList.map((activity) => (
-                    <Activity key={activity.id} title={activity.title} cardsData={activity.cardsData} />
-                ))}
+                <Activity key={latestProducts.id} title={latestProducts.title} cardsData={latestProducts.cardsData}/>
+                <Activity key={latestRequests.id} title={latestRequests.title} cardsData={latestRequests.cardsData}/>
             </div>
         </div>
     );
 }
-
-
-
 
 export default Dashboard;
