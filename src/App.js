d928ebc73c269b553c6eb42b6c75995dbd6f9443
Amr Yousef <3mr.g4mer.channel@gmail.com>
2023-05-09 01:11:16 +0300

    feat: redirect notification page to requests

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import "./styles/App.css"
-import  Header  from "./components/Header";
+import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import  Content  from "./components/Content";
+import Content from "./components/Content";
 
 import Login from "./components/Login";
 
-function handleLogin(){
+function handleLogin() {
     return localStorage.getItem('loggedIn') === 'true';
 }
-function handleType(){
-    return localStorage.getItem('sessionID') !== '1';
-}
-function App(){
-    if(handleLogin()) {  // if user is logged in
-        if(handleType()){
-           return( <div className="App">
+
+function App() {
+    if (handleLogin()) {  // if user is logged in
+        return (<div className="App">
             <Header/>
             <Sidebar/>
-            <Content />
+            <Content/>
         </div>);
-        }else{
-            return(
-        <div className="App">
-            <Header/>
-            <Sidebar/>
-            <Content />
-        </div>);
-        }
-        
     }
     return (<div>
-            <Login/>
-        </div>);
+        <Login/>
+    </div>);
 }
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
-import { Badge} from "antd";
-import {BellFilled,UserOutlined} from '@ant-design/icons';
-function Header(){
+import { Badge } from "antd";
+import { BellFilled, UserOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    const requestPage = localStorage.getItem("sessionID") == 1 ? "/Request" : "/Supervisor/Requests";
     return (
         <div className="AppHeader">
-   
-       <div className="header-right">
-       <Badge count={20}>
-        <BellFilled style={{fontSize: 24 , cursor:"pointer"}}/>
-        </Badge>
-        
-        <UserOutlined className="icon-header-right"/>
-       </div>
-        
-      
-        
+            <div className="header-right">
+                <Link to={requestPage}>
+                    <Badge count={20}>
+                        <BellFilled style={{ fontSize: 24, cursor: "pointer" }} />
+                    </Badge>
+                </Link>
+                <UserOutlined className="icon-header-right" aria-label="User Icon" />
+            </div>
         </div>
-    )
+    );
 }
 
 export default Header;
